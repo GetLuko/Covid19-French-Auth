46afcb2591e4dd95b814268a72cd2c955009dffc
Ben <benoit@getluko.com>
2020-10-30 09:24:48 +0100

    Update with Fall 2020

--- generate.js
+++ generate.js
@@ -10,23 +10,23 @@ window.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById("field-firstname").value = urlParams.get("f"); //firstname
     document.getElementById("field-lastname").value = urlParams.get("l"); //lastname
     document.getElementById("field-birthday").value = urlParams.get("b"); //birthday
-    document.getElementById("field-lieunaissance").value = urlParams.get("p"); //place of born
+    document.getElementById("field-placeofbirth").value = urlParams.get("p"); //place of born
     document.getElementById("field-address").value = urlParams.get("a"); //address
-    document.getElementById("field-town").value = urlParams.get("c"); //city
+    document.getElementById("field-city").value = urlParams.get("c"); //city
     document.getElementById("field-zipcode").value = urlParams.get("z"); //zipcode
     const reason = urlParams.get("r"); //reason
     document.getElementById("radio-travail").checked = (reason==='travail');
+    document.getElementById("radio-achats").checked = (reason==='achats');
+    document.getElementById("radio-enfants").checked = (reason==='enfants');
+    document.getElementById("radio-sport_animaux").checked = (reason==='sport_animaux');
     document.getElementById("radio-sante").checked = (reason==='sante');
     document.getElementById("radio-famille").checked = (reason==='famille');
     document.getElementById("radio-handicap").checked = (reason==='handicap');
     document.getElementById("radio-convocation").checked = (reason==='convocation');
-    document.getElementById("radio-missions").checked = (reason==='missions');
-    document.getElementById("radio-transits").checked = (reason==='transits');
-    document.getElementById("radio-animaux").checked = (reason==='animaux');
 
     var now = new Date()
     document.getElementById('field-datesortie').value = now.toJSON().slice(0,10);
-    document.getElementById('field-heuresortie').value = now.getHours()+":"+now.getMinutes();
+    document.getElementById('field-heuresortie').value = now.toJSON().slice(11,16);
 
 
     document.getElementById("generate-btn").click()
