b04d277b40dd852962f0daf09cc279404466a624
Thomas Mosmant <thomas@luko.eu>
2020-11-05 11:21:35 +0100

    Update certificate

--- generate.js
+++ generate.js
@@ -43,7 +43,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
 function badScriptLoading(event) {
     console.log("Official JS is not working. Patching using local copy.")
     let script = document.createElement('script');
-    script.src = "deplacement-covid-19/main.d56e3230.js";
+    script.src = "deplacement-covid-19/main.02381a6a.js";
     document.head.append(script)
 }
 
