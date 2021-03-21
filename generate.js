7cddc18d382aa62012de0adae8a3c1c65c93226b
tmosmant <thomas.mosmant@gmail.com>
2021-03-21 17:58:48 +0000

    Deploying to gh-pages from @ GetLuko/Covid19-French-Auth@843a178728188b3f648ec749d59ad0d3f227a9c4 🚀

--- generate.js
+++ generate.js
@@ -13,7 +13,15 @@ window.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById("field-city").value = urlParams.get("c"); //city
     document.getElementById("field-zipcode").value = urlParams.get("z"); //zipcode
 
-    const reason = urlParams.get("r"); //reason
+    let reason = urlParams.get("r"); //reason
+    const isQuarantine = reason.indexOf("quarantine-") !== -1;
+    reason = reason.replace(/curfew-|quarantine-/, '')
+    if (isQuarantine) {
+        document.getElementById("quarantine-button").click()
+    } else {
+        document.getElementById("curfew-button").click()
+    }
+
     document.getElementById("radio-travail").checked = (reason==='travail');
     document.getElementById("radio-sante").checked = (reason==='sante');
     document.getElementById("radio-famille").checked = (reason==='famille');
@@ -22,6 +30,15 @@ window.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById("radio-missions").checked = (reason==='missions');
     document.getElementById("radio-transits").checked = (reason==='transits');
     document.getElementById("radio-animaux").checked = (reason==='animaux');
+
+    document.getElementById("radio-sport").checked = (reason==='sport');
+    document.getElementById("radio-achats").checked = (reason==='achats');
+    document.getElementById("radio-enfants").checked = (reason==='enfants');
+    document.getElementById("radio-culte_culturel").checked = (reason==='culte_culturel');
+    document.getElementById("radio-demarche").checked = (reason==='demarche');
+    document.getElementById("radio-judiciaire").checked = (reason==='judiciaire');
+    document.getElementById("radio-demenagement").checked = (reason==='demenagement');
+    document.getElementById("radio-transit").checked = (reason==='transit');
 
     var now = new Date()
     var timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone || "Europe/Paris"
@@ -45,7 +62,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
 function badScriptLoading(event) {
     console.log("Official JS is not working. Patching using local copy.")
     let script = document.createElement('script');
-    script.src = "attestation-couvre-feu-covid-19/main.36f6dadf.js";
+    script.src = "attestation-deplacement-derogatoire-covid-19/main.561be803.js";
     document.head.append(script)
 }
 
