7699c995dfcbc52a057a3416869432b8995aa6eb
Thomas Mosmant <thomas.mosmant@gmail.com>
2021-03-25 20:41:14 +0100

    Test version to spot official updates

---
+++ tests/version.test.js
@@ -0,0 +1,21 @@
+const { chromium } = require('playwright');
+
+describe("Check official version", () => {
+  it("should have the exact same version", async () => {
+    const browser = await chromium.launch();
+    const context = await browser.newContext();
+  
+    // Open new page
+    const page = await context.newPage();
+  
+    // Go to https://media.interieur.gouv.fr/attestation-deplacement-derogatoire-covid-19/
+    await page.goto('https://media.interieur.gouv.fr/attestation-deplacement-derogatoire-covid-19/');
+  
+    // Click text=2021 - 2.4.1
+    await page.click('text=2021 - 2.4.1');
+  
+    // ---------------------
+    await context.close();
+    await browser.close();
+  })
+})
