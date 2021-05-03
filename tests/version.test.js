62812d9ff9b88f182a80d02a512ea2a0fd30a00d
tmosmant <thomas.mosmant@gmail.com>
2021-05-03 12:06:07 +0000

    Deploying to gh-pages from @ GetLuko/Covid19-French-Auth@3e619ee07ac6e5796af4113edfee2a9cec098257 🚀

--- tests/version.test.js
+++ tests/version.test.js
@@ -1,21 +1,23 @@
-const { chromium } = require('playwright');
+const { chromium } = require("playwright");
 
 describe("Check official version", () => {
   it("should have the exact same version", async () => {
     const browser = await chromium.launch();
     const context = await browser.newContext();
-  
+
     // Open new page
     const page = await context.newPage();
-  
+
     // Go to https://media.interieur.gouv.fr/attestation-deplacement-derogatoire-covid-19/
-    await page.goto('https://media.interieur.gouv.fr/attestation-deplacement-derogatoire-covid-19/');
-  
-    // Click text=2021 - 2.5.3
-    await page.click('text=2021 - 2.5.3');
-  
+    await page.goto(
+      "https://media.interieur.gouv.fr/attestation-deplacement-derogatoire-covid-19/"
+    );
+
+    // Click text=2021 - 2.6.0
+    await page.click("text=2021 - 2.6.0");
+
     // ---------------------
     await context.close();
     await browser.close();
-  })
-})
+  });
+});
